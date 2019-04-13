5fc85c0486f0df3b45ed87b59528773b32811d70
harkhuang <shiyanhk@gmail.com>
2019-04-13 23:39:25 +0800

    mod

--- python/study_myself/13_class/5_.py
+++ python/study_myself/13_class/5_.py
@@ -1,10 +1,38 @@
-class AbstractClass:
-    
-    def do_something(self):
-        pass
-    
-    
-class B(AbstractClass):
-    pass
-a = AbstractClass()
-b = B()
+from abc import ABCMeta
+
+class MyABC:
+    __metaclass__ = ABCMeta
+
+MyABC.register(tuple)
+
+assert issubclass(tuple, MyABC)
+assert isinstance((), MyABC)
+
+
+class Foo(object):
+    def __getitem__(self, index):
+        self.index = 'xx'
+    def __len__(self):
+        return len(self.index)
+    def get_iterator(self):
+        return iter(self)
+
+class MyIterable:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __iter__(self):
+        while False:
+            yield None
+
+    def get_iterator(self):
+        return self.__iter__()
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is MyIterable:
+            if any("__iter__" in B.__dict__ for B in C.__mro__):
+                return True
+        return NotImplemented
+
+MyIterable.register(Foo)
