608ae194810823890b53659c46198c1e10514769
shiyanhk@gmail.com <shiyanhk@gmail.com>
2019-04-17 15:02:56 +0800

    mod

---
+++ cpp/template/1.cpp
@@ -0,0 +1,34 @@
+/* *****
+模板显示特化
+**** */
+
+
+#include <iostream>
+
+template <class T>
+struct CTest{
+    void operator()(){ std::cout<< "CTest<T>"<<std::endl;}
+};
+
+struct CTest<char > {
+    void operator()(){ std::cout<< "CTest<char>"<<std::endl;}
+};
+
+struct CTest<int > {
+    void operator()(){ std::cout<< "CTest<int>"<<std::endl;}
+};
+
+
+int main()
+{
+    CTest<double> test1;
+    CTest<char> test2;
+    CTest<int> test3;
+    
+
+    test1();
+    test2();
+    test3();
+
+    return 0;
+}
