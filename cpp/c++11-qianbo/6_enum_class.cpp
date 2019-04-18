80fee8300e7edf988a77157acc5bc34ea0cf74f1
shiyanhk@gmail.com <shiyanhk@gmail.com>
2019-04-18 16:48:23 +0800

    mod

--- cpp/c++11-qianbo/6_enum_class.cpp
+++ cpp/c++11-qianbo/6_enum_class.cpp
@@ -13,42 +13,30 @@ void test03(){
    enum orange {big_o, small_o};
    apple a = green_a;
    orange o = big_o;
-
    if (a == o) 
       cout << "green apple and big orange are the same\n";
    else
       cout << "green apple and big orange are not the same\n";
-
 }
 
 
 
 void test11(){
-   
    // C++ 11
    enum class apple {green, red};
    enum class orange {big, small};
-   
    apple a = apple::green;
    orange o = orange::big;
-
    // if (a == o) 
    //    cout << "green apple and big orange are the same\n";
    // else
    //    cout << "green apple and big orange are not the same\n";
-
    // Compile fails because we haven't define ==(apple, orange)
-
 }
 int main()
 {
    test03();
    test11();
-
    return 0;
-
-
-
-
 }
 
