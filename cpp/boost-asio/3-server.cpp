4aa2153377b991ae7ce700e85b2c58d6804deef7
harkhuang <shiyanhk@gmail.com>
2019-07-10 23:40:36 +0800

    update asio and c11

--- cpp/boost-asio/3-server.cpp
+++ cpp/boost-asio/3-server.cpp
@@ -1,14 +1,30 @@
 #include <boost/asio.hpp> 
 #include <iostream>
 
-int main( int argc, char * argv[] )
-{
+
+
+
+// no block
+void test1(){
+	boost::asio::io_service io_service;	
+
+		boost::asio::io_service::work work( io_service );
+	//for(int i=0;i<5;++i){
+		io_service.run();
+	//}
+	std::cout << "Do you reckon this line displays?" << std::endl;
+}
+// no block
+void test2(){
 	boost::asio::io_service io_service;
 	boost::asio::io_service::work work( io_service );
-
-	io_service.run();
-
+	for(int i=0;i<5;++i){
+		io_service.run();
+	}
 	std::cout << "Do you reckon this line displays?" << std::endl;
-
+}
+int main( int argc, char * argv[] )
+{
+	test1();
 	return 0;
 }
