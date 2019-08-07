d37cca6f5045ddd8d1c7d89d6e713891c85c95ce
harkhuang <shiyanhk@gmail.com>
2019-08-07 10:18:11 +0800

    update

---
+++ cpp/c++11/constexpr.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdexcept>
+ 
+// C++11 constexpr functions use recursion rather than iteration
+// (C++14 constexpr functions may use local variables and loops)
+
+
+// 常量传递 在编译器被展开并且计算
+// 递归函数在编译器间被展开计算
+constexpr int factorial(int n)
+{
+    return n <= 1 ? 1 : (n * factorial(n - 1));
+}
+ 
+// literal class
+class conststr {
+    const char* p;
+    std::size_t sz;
+public:
+    template<std::size_t N>
+    constexpr conststr(const char(&a)[N]): p(a), sz(N - 1) {}
+ 
+    // constexpr functions signal errors by throwing exceptions
+    // in C++11, they must do so from the conditional operator ?:
+    constexpr char operator[](std::size_t n) const
+    {
+        return n < sz ? p[n] : throw std::out_of_range("");
+    }
+    constexpr std::size_t size() const { return sz; }
+};
+ 
+// C++11 constexpr functions had to put everything in a single return statement
+// (C++14 doesn't have that requirement)
+constexpr std::size_t countlower(conststr s, std::size_t n = 0,
+                                             std::size_t c = 0)
+{
+    return n == s.size() ? c :
+           'a' <= s[n] && s[n] <= 'z' ? countlower(s, n + 1, c + 1) :
+                                       countlower(s, n + 1, c);
+}
+ 
+// output function that requires a compile-time constant, for testing
+template<int n>
+struct constN
+{
+    constN() { std::cout << n << '\n'; }
+};
+ 
+int main()
+{
+    std::cout << "4! = " ;
+    constN<factorial(4)> out1; // computed at compile time
+ 
+    volatile int k = 8; // disallow optimization using volatile
+    std::cout << k << "! = " << factorial(k) << '\n'; // computed at run time
+ 
+    std::cout << "the number of lowercase letters in \"Hello, world!\" is ";
+    constN<countlower("Hello, world!")> out2; // implicitly converted to conststr
+}
+Output:
+
+4! = 24
+8! = 40320
+the number of lowercase letters in "Hello, world!" is 9
