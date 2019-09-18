9583a1b6bb7959ad2d9b6b3b37fbfa5e67940e10
harkhuang <shiyanhk@gmail.com>
2019-09-18 19:44:00 +0800

    update

--- cpp/boost-asio/0-server.cpp
+++ cpp/boost-asio/0-server.cpp
@@ -26,6 +26,57 @@ int test_1(  )
 	return 0;
 }
 
+int test_3(  )
+{
+	boost::asio::io_service io_service;
+	boost::shared_ptr< boost::asio::io_service::work > work(
+		new boost::asio::io_service::work( io_service )
+	);
+
+	work.reset();
+
+	io_service.run();
+
+	std::cout << "Do you reckon this line displays?" << std::endl;
+
+	return 0;	
+}
+
+
+
+boost::asio::io_service io_service;
+
+void WorkerThread()
+{
+	std::cout << "Thread Start\n";
+	io_service.run();
+	std::cout << "Thread Finish\n";
+}
+
+int test_4()
+{
+	boost::shared_ptr< boost::asio::io_service::work > work(
+		new boost::asio::io_service::work( io_service )
+	);
+
+	std::cout << "Press [return] to exit." << std::endl;
+
+	boost::thread_group worker_threads;
+	for( int x = 0; x < 4; ++x )
+	{
+		worker_threads.create_thread( WorkerThread );
+	}
+
+	std::cin.get();
+
+	io_service.stop();
+
+	worker_threads.join_all();
+
+	return 0;
+}
+
+ 
 
 int main()
 {
