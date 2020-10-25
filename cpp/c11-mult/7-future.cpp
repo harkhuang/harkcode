7597725d0102ed4b8e30b164a69bcfcb022b99c4
harkhuang <shiyanhk@gmail.com>
2020-10-26 01:22:48 +0800

    update 1026

--- cpp/c11-mult/7-future.cpp
+++ cpp/c11-mult/7-future.cpp
@@ -11,20 +11,22 @@ static void factorial(int N ,int &ref)
     for(int i= N; i>1;i--){
         res *= i;
     }
-
     cout<< "result is :"<< res <<endl;
     ref = res;
 }
 
 
 
-
-
-
 int main()
 {
     int a = 0;
+
+    // 这里第二个 参数表示 future 执行的时间点
+    // 延迟到最后在执行 
     future<int > fu = std::async(std::launch::deferred, factorial, 4, a);
+
+
+    // 等待异步执行完毕
     auto x = fu.get();
     
     return 0;
