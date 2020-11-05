0414e4c08d70df49d586ab9fa22131bcc7a7cedd
harkhuang <shiyanhk@gmail.com>
2020-11-06 00:43:43 +0800

    update

--- cpp/c++11/test/2-initializer_list.cpp
+++ cpp/c++11/test/2-initializer_list.cpp
@@ -1,57 +1,56 @@
-/*
- * 2. Uniform Initialization
- */
 #include <iostream>
 #include <vector>
-
-// 匿名初始化构造  
-#include <initializer_list> 
-// C++ 03
-class Dog
- {     // Aggregate class or struct
-   public:
-      int age;
-     std::string name;
+#include <initializer_list>
+ 
+template <class T>
+struct S {
+    std::vector<T> v;
+    S(std::initializer_list<T> l) : v(l) {
+         std::cout << "constructed with a " << l.size() << "-element list\n";
+    }
+    void append(std::initializer_list<T> l) {
+        v.insert(v.end(), l.begin(), l.end());
+    }
+    std::pair<const T*, std::size_t> c_arr() const {
+        return {&v[0], v.size()};  // copy list-initialization in return statement
+                                   // this is NOT a use of std::initializer_list
+    }
 };
-
-Dog d0 ;
-d0.age = 5;
-d
-
-Dog d0 = {5, "Henry"};   // Aggregate Initialization
-
-// C++ 11 extended the scope of curly brace initialization
-class Dog1 {
-   public:
-      Dog1(int age, std::string name) {};
-};
-Dog1 d1 = {5, "Henry"}; 
+ 
 
 
-/* Uniform Initialization Search Order:
- * 1. Initializer_list constructor
- * 2. Regular constructor that takes the appropriate parameters.
- * 3. Aggregate initializer.
- */
-
-Dog d1{3};
-
-class Dog {
-   public:
-   int age;                                // 3rd choice
-
-   Dog(int a) {                            // 2nd choice
-      age = a;
-   }
-
-   Dog(const std::initializer_list<int>& vec) { // 1st choice
-      age = *(vec.begin());      
-   }
-};
-
-
-
+ // 在预编译过程中编译器对其展开和解释,
+template <typename T>
+void templated_fn(T) {}
+ 
 int main()
 {
-   return 0;
+
+   // 构造函数使用模版  就可以用各种类型构建
+    S<int> s = {1, 2, 3, 4, 5}; // copy list-initialization
+    s.append({6, 7, 8});      // list-initialization in function call
+ 
+    std::cout << "The vector size is now " << s.c_arr().second << " ints:\n";
+ 
+    for (auto n : s.v)
+        std::cout << n << ' ';
+    std::cout << '\n';
+ 
+    std::cout << "Range-for over brace-init-list: \n";
+ 
+
+
+   // 无名的初始化
+    for (int x : {-1, -2, -3}) // the rule for auto makes this ranged-for work
+        std::cout << x << ' ';
+    std::cout << '\n';
+ 
+    auto al = {10, 11, 12};   // special rule for auto
+ 
+    std::cout << "The list bound to auto has size() = " << al.size() << '\n';
+ 
+//    templated_fn({1, 2, 3}); // compiler error! "{1, 2, 3}" is not an expression,
+                             // it has no type, and so T cannot be deduced
+    templated_fn<std::initializer_list<int>>({1, 2, 3}); // OK
+    templated_fn<std::vector<int>>({1, 2, 3});           // also OK
 }
